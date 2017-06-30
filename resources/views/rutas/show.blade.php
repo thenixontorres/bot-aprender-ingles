6bf56fe98ee84d6522a0bdaec499d6b4df996446
Nixon <nixontorres26@gmail.com>
2017-06-30 00:33:16 -0400

    reporte de rutas

--- resources/views/rutas/show.blade.php
+++ resources/views/rutas/show.blade.php
@@ -1,65 +1,46 @@
 @extends('layouts.doc')
 	@section('content')
-	<h1>{{ $ruta->direccion.', '.$ruta->empleado->nombre.' '.$ruta->empleado->apellido.' '.$ruta->empleado->cedula }}</h1>
+	
+	<div class="col-md-12">
+		<img class="img-responsive" style="min-width: 100%" src="{{asset('img/logo-recibo.jpg')}}">
+	</div>
+	<h4>{{ 'Ruta: '.$ruta->direccion.', '.$ruta->empleado->nombre.' '.$ruta->empleado->apellido.' '.$ruta->empleado->cedula }}</h4>
+	<h4>{{ 'Cobrador: '.$ruta->empleado->nombre.' '.$ruta->empleado->apellido }}</h4>
 	<table class="table table-bordered">
 	<thead>
 	<th>Nº Contrato</th>
+	<th>Cedula</th>
 	<th>Titular</th>
 	<th>Direccion</th>
-	<th>Plan</th>
-	<th>Monto a Pagar</th>
+	<th>Telefono</th>
+	<th>Cuotas por Cobrar</th>
+	<th>Monto</th>
 	<th>Visitado</th>
 	</thead>
 	<tbody>
 		@foreach($ruta->contratos as $contrato)
 		    @if($contrato->estado == 'Activo')
+		    	@foreach($contrato->personas as $persona)
+		        	@if($persona->parentesco == 'Titular')
+			        	<?php  $titular = $persona; ?>
+		        	@endif
+		        @endforeach
 		    <tr>
 		    <td> {{ $contrato->numero}} </td>
 		    <td>
-		        @foreach($contrato->personas as $persona)
-		        	@if($persona->parentesco == 'Titular')
-		        	{!! $persona->nombre.' '.$persona->apellido.' '.$persona->cedula !!}
-		        	@endif
-		        @endforeach
+		        {{ $titular->cedula }}
 		    </td>
+		    <td> {{ $titular->nombre.' '.$titular->apellido }}</td>
 		    <td>
-		        @foreach($contrato->personas as $persona)
-		        	@if($persona->parentesco == 'Titular')
-		        	{!! $persona->direccion !!}
-		        	@endif
-		        @endforeach
+		        {{ $titular->direccion }}
 		    </td>
-		    <td>{!!  $contrato->plan->plan !!}</td>
-		    <?php  
-		        if($contrato->tiempo_pago == "Quincenal"){
-		            $cuota_mensual = $contrato->tiempo_pago = 24;
-		        }elseif($contrato->tiempo_pago == "Semanal"){
-		            $cuota_mensual = $contrato->tiempo_pago = 12;
-		        }else{
-		            $cuota_mensual = $contrato->tiempo_pago = 6;
-		        }      
-		    ?>
-		    <td> {{ 'Bs: '.@number_format($cuota_mensual, 2, ',', ' ') }}</td>
+		    <td>{{ $titular->telefono }}</td>
+		   	<td>--</td>
+		   	<td>--</td>
+		    <td>--</td>
 		    <td></td>
 		    @endif
 		@endforeach	
 	</tbody>
 	</table>
-	<table class="table table-bordered">
-		<th>
-	        NOTA:
-	        <br>
-	        <p>1. Favor los pagos funerarios en cheques hacerlo a nombre de: Servicios Funerarios y Previsivos Virgen de Coromoto C.A </p>
-	        <p>2. La primera cuota se cobrara a los treinta (30) dias de la firma de este convenio.</p>
-	        <p>3. Se cobrara un recargo en caso de cheque devuelto.</p>
-	        <p>4. Este convenio no tendra validez si presenta enmienda o datos falsos.
-	    </th>
-	    <th>
-	   		<p class="text-center">Aprobado Por</p>
-	        <p>Servicios Funerarios y </p>
-	        <p>Previsivos Virgen de </p>
-	        <p>Coromoto C.A </p>
-	        <p>______________________</p>
-	    </th>    
-    </table>
 @endsection
